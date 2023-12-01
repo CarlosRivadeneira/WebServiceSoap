86717e7cfa1f5db6541dddbb713eb7f775d56df9
CarlosRivadeneira <carlosrvda@gmail.com>
2023-11-30 20:34:42 -0500

    Agregar archivos de proyecto.

---
+++ Client/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Service.ServiceClient oClient = new Service.ServiceClient();
+            string res = oClient.GetData(5,3);
+            MessageBox.Show(res);
+        }
+    }
+}
